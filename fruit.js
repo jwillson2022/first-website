22a3881cda82b8c5a89b826cb50b83be0ac54974
jonathanwillson2022 <drawnbydog@gmail.com>
2022-05-14 16:10:23 +0100

    added fruit
    fruit.js
    fruit.html;

---
+++ fruit.js
@@ -0,0 +1,12 @@
+
+
+
+let element_banana = document.getElementById('banana');
+element_banana.style.color = '#ffff33';
+element_banana.style.backgroundColor = '#000000';
+let element_apple = document.getElementById('apple');
+element_apple.style.color = '#33cc00';
+element_apple.style.backgroundColor = '#000000';
+let element_strawberry = document.getElementById('strawberry');
+element_strawberry.style.color = '#ff0000';
+element_strawberry.style.backgroundColor = '#000000';
